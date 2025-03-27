68b30572e57e4556e04401034230ed271d7ff553
Atkenny <tellezkenny08@gmail.com>
2025-03-27 09:20:50 -0600

    Boton de editar en Tarjeta de productos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./assets/database/authcontext";
-import ProtectedRoute from "./assets/components/ProtectedRoute"; 
-import Login from './assets/components/Login'
-import Encabezado from "./assets/components/Encabezado";
-import Inicio from "./assets/components/Inicio";
+import { AuthProvider } from "../src/assets/database/authcontext";
+import ProtectedRoute from "./components/ProtectedRoute"; 
+import Login from './views/Login'
+import Encabezado from "./components/Encabezado";
+import Inicio from "./views/Inicio";
+import Categorias from "./views/Categorias";
+import Productos from "./views/Productos";
+import Catalogo from "./components/Catalogo/Catalogo";
 
 import './App.css'
 
 function App() {
-
   return (
-    <>
-      <AuthProvider>
-        <Router>
-          <div className="App">
-            <Encabezado />
-            <main>
-              <Routes>
-                
-                <Route path="/" element={<Login />} />
-                <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
-
-              </Routes>
-            </main>
-          </div>
-        </Router>
-      </AuthProvider>
-    </>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Encabezado />
+          <main className="margen-superior-main">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
+              <Route path="/categorias" element={<ProtectedRoute element={<Categorias />} />}/>
+              <Route path="/productos" element={<ProtectedRoute element={<Productos />} />}/>
+              <Route path="/catalogo" element={<ProtectedRoute element={<Catalogo />} />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </AuthProvider>
   )
 }
 
-export default App
+export default App;
